4046a6cb5755ebf3419a2cb94da01593802cc046
Filipe Bessa <filipegbessa@gmail.com>
2021-10-30 18:35:33 -0300

    feat(package.json): install and/or configure husky, commitzen, commitlint and eslit and fix errors
    Install and/or configure Husky, Commitzen, Commitlint and ESLINT and fix linting errors

---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react";
+import { AppProps } from "next/app";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default App;
